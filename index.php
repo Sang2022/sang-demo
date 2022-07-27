7dca1dfee2ec2df233a8a5eefcc2e311be5455af
Sang2022 <109645730+Sang2022@users.noreply.github.com>
2022-07-27 13:21:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php include_once("home.html"); ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>
+
+<?php
+$url='https://vnexpress.net/rss/tin-moi-nhat.rss';
+$lines_array=file($url);
+$lines_string=implode('',$lines_array);
+
+$xml = simplexml_load_string($lines_string);
+if ($xml === false) {
+    echo "Failed loading XML: ";
+    foreach(libxml_get_errors() as $error) {
+        echo "<br>", $error->message;
+    }
+}else{
+    echo $xml->asXML();
+}
+?>
